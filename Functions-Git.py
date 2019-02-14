79364320134c9535f6f866debffe442825904100
Syann Hollins <02syann.hollins@gulllakecs.org>
2019-02-14 09:33:47 -0500

    Add Functions and Variables

--- Functions-Git.py
+++ Functions-Git.py
@@ -2,4 +2,25 @@
 # Syann Hollins
 # 2.6.19
 
-name =
+'''
+name = input("What is your name: ")
+
+def greetings():
+    print("Hi there " + name + "!")
+    print("Nice to meet you!")
+
+greeting()
+'''
+
+# 4.13.4: Functions and Variables
+# Syann Hollins
+# 2.14.19
+
+x = 406
+
+def print_something():
+    x = 13
+    print(x)
+
+print_something()
+print(x)

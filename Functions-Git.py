bc1957955b432811c2bab3c1f1e198c09e7d6c55
Syann Hollins <02syann.hollins@gulllakecs.org>
2019-02-27 09:59:39 -0500

    Added Functions & Variables, Part 3

--- Functions-Git.py
+++ Functions-Git.py
@@ -2,7 +2,7 @@
 # Syann Hollins
 # 2.6.19
 
-'''
+
 name = input("What is your name: ")
 
 def greetings():
@@ -10,7 +10,7 @@ def greetings():
     print("Nice to meet you!")
 
 greeting()
-'''
+
 
 # 4.13.4: Functions and Variables
 # Syann Hollins
@@ -24,3 +24,27 @@ def print_something():
 
 print_something()
 print(x)
+
+
+# 4.13.5: Functions and Variables-part 2
+# Syann Hollins
+# 2.14.19
+
+my_variable = 3.6745
+
+def something():
+    print(my_variable + 10)
+
+something()
+
+
+# 4.13.6: Functions & variables, part 3
+# Syann Hollins
+# 2.18.19
+
+
+def print_number(x):
+    print(str(x))
+
+print_number(12)
+print_number('\n' + 'Hello World')
